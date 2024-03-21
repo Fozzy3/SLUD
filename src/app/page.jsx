cb3ba626ce31af764e176dba8668d8456afe4fc0
sergio-paez <sergio.paez@puntored.co>
2024-03-20 19:00:36 -0500

    change background intro section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
           <h1 className='text-transparent bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-5xl font-bold lg:tracking-tight xl:tracking-tighter'>
             Semana Linux Universidad Distrital
           </h1>
-          <p className="text-lg mt-4 text-gray-400 max-w-xl text-justify">
+          <p className="text-lg mt-4 text-black-400 max-w-xl text-justify">
             La XX Edición del Evento de Software Libre, organizado por el grupo GLUD, se celebra en la Universidad
             Distrital Francisco José de Caldas en Bogotá, Colombia. Este año nos enfocamos en la Ciencia de Datos
             y su relación con el software libre. Invitamos a expertos en ciencia de datos con herramientas de código abierto a unirse
@@ -18,7 +18,7 @@ export default function Home() {
           </p>
         </div>
         <div className={`${styles.start_section_image}`}>
-          <Image src="logo-slud-xxi.svg"  
+          <Image src="/data_2.png"  
             height={200}
             width={200}
             alt="slud logo"
