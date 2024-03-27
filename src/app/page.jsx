2d22881d57a7658f076dd794961901e171f61122
sergio-paez <sergio.paez@puntored.co>
2024-03-27 11:53:14 -0500

    fix: style some components in landing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,13 @@
-import SponsorSection from '@/components/other/sponsor/SponsorSection';
-import SpeakerSection from '@/components/other/speaker/SpeakerSection';
-import StartSection from '@/components/other/start/StartSection';
+import SponsorSection from '@/components/other/landing/sponsor/SponsorSection';
+import SpeakerSection from '@/components/other/landing/speaker/SpeakerSection';
+import StartSection from '@/components/other/landing/start/StartSection';
 import styles from './main.module.css';
 
 export default function Home() {
   return (
     <main className={`${styles.main_wrapper}`}>
-      {/*Seccion de inicio*/}
       <StartSection />
-      {/*Seccion de sponsors*/}
       <SponsorSection />
-      {/*Seccion de speakers*/}
       <SpeakerSection />
     </main>
   );
