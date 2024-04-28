35a07acba809a781a2fc803795bbb02ff261b060
Ludwind Deward Rico Rodríguez <zodjek95@gmail.com>
2024-04-27 19:04:04 -0500

    version 1.2 de footer

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -9,18 +9,9 @@ const Footer = () => {
 
         <div id="upper_part_footer" className={`${styles.footer_up}`}>
           <img 
-            src={"./logo_ud.png"} 
-            alt={"logo de la universidad distrital"}
+            src={"./logo_ud_blanco.png"} 
+            alt={"escudo de la universidad distrital"}
           />
-          <div id="text_footer_up" className={`${styles.top}`}>
-            <h3>
-              UNIVERSIDAD DISTRITAL
-              FRANCISCO JOSÉ DE CALDAS
-            </h3>
-            <h2>
-              Acreditación institucional de alta calidad
-            </h2>
-          </div>
         </div>
 
         <div id="footer_row" className={`${styles.footer_row}`}>
@@ -30,10 +21,10 @@ const Footer = () => {
             <h4>Grupo</h4>
             <ul id="lista_grupo" className={`${styles.listas}`}>
 
-              <li><a href="#">We</a></li>
-              <li><a href="#">Historia</a></li>
-              <li><a href="#">Tema</a></li>
-              <li><a href="#">Tematica</a></li>
+              <li><a href="https://glud.udistrital.edu.co/" target="_blank">We</a></li>
+              <li><a href="https://www.youtube.com/watch?v=hGl7ShlXMd0" target="_blank">Historia</a></li>
+              <li><a href="https://www.mistralbs.com/blog/analisis-y-ciencia-de-datos-origenes-e-historia/" target="_blank">Tema</a></li>
+              <li><a href="https://gitlab.com/GLUD" target="_blank">Tematica</a></li>
 
             </ul>
 
@@ -43,33 +34,46 @@ const Footer = () => {
 
             <h4>Donde Encontrarnos</h4>
 
+
+            <div className={`${styles.social_links}`}>
+
+              <a href="https://www.facebook.com/GrupoLinuxUD" target="_blank"><i id="f-facebook" className="fab fa-facebook" alt="Icono de facebook"></i></a>
+              <a href="https://www.instagram.com/grupo_glud/" target="_blank"><i id="f-instagram" className="fab fa-instagram" alt="Icono de instagram"></i></a>
+              <a href="https://twitter.com/GrupoGLUD" target="_blank"><i id="f-twitter" className="fab fa-twitter" alt="Icono de twitter"></i></a>
+              <a href="https://www.linkedin.com/in/grupoglud/" target="_blank"><i id="f-linkedin" className="fab fa-linkedin" alt="Icono de linkedin"></i></a>
+              <a href="https://www.youtube.com/c/GrupoLinuxUniversidadDistrital" target="_blank"><i id="f-youtube" className="fab fa-youtube" alt="Icono de youtube"></i></a>
+              <a href="https://www.twitch.tv/grupoglud" target="_blank"><i id="f-twitch" className="fab fa-twitch" alt="Icono de twitch"></i></a>
+              <a href="https://www.tiktok.com/@grupo_glud" target="_blank"><i id="f-tiktok" className="fab fa-tiktok" alt="Icono de tik tok"></i></a>
+
+            </div>
+
+
             <a href="https://www.google.com/maps/place/Universidad+Distrital+Calle+40/@4.627963,-74.065937,21z/data=!4m6!3m5!1s0x8e3f9a286d7ef055:0x9cee6f53b8b5f090!8m2!3d4.6279572!4d-74.0659162!16s%2Fg%2F11bw4q7_lt?hl=es&entry=ttu"
             target="_blank">
               <img src="./ubicacion.png" alt="enlace a nuestra ubicacion"/>
             </a>
-          </div>
 
+
+          </div>
 
           <div id="siguenos" className={`${styles.footer_links}`}>
 
-            <h4>Síguenos</h4>
-            <div className={`${styles.social_links}`}>
+            <h4>Trabajo</h4>
+            <ul id="lista_grupo" className={`${styles.listas}`}>
 
-              <a href="https://www.facebook.com/GrupoLinuxUD/?locale=es_LA"><i id="f-facebook" className="fab fa-facebook" alt="Icono de facebook"></i></a>
-              <a href="https://glud.udistrital.edu.co"><i id="f-instagram" className="fab fa-instagram" alt="Icono de instagram"></i></a>
-              <a href="https://slud.udistrital.edu.co"><i id="f-twitter" className="fab fa-twitter" alt="Icono de twitter"></i></a>
-              <a href="#"><i id="f-linkedin" className="fab fa-linkedin" alt="Icono de linkedin"></i></a>
-              <a href="#"><i id="f-youtube" className="fab fa-youtube" alt="Icono de youtube"></i></a>
+              <li><a href="https://github.com/GLUD" target="_blank">Github</a></li>
+              <li><a href="https://gitlab.com/GLUD" target="_blank">Gitlab</a></li>
 
-            </div>
-
+            </ul>
           </div>
 
         </div>
 
 
         <div id="destino_final" className={`${styles.foondo}`}>
-          <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script>. GLUD</p>
+          <pre>GLUD UD      contacto:
+            glud@udistrital.edu.co      Copyright &copy;
+            2024</pre>
         </div>
       </div>
 
